5e302cb4fb38e13998ef8f4e9f1b6694b5cb7b58
Bharath kumar <bharathboopathi51550@gmail.com>
2024-04-21 17:01:42 +0530

    loading add

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { toast } from "react-toastify";
@@ -8,12 +8,13 @@ import { useNavigate } from "react-router-dom";
 
 function Signin() {
   let navigate = useNavigate();
+  let [load,setLoad]=useState(false)
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
       const formData = new FormData(e.target);
       const formProps = Object.fromEntries(formData);
-
+      setLoad(true)
       let res = await AxiosService.post(
         `${ApiRoutes.SIGN_IN.path}`,
         formProps,
@@ -22,8 +23,9 @@ function Signin() {
         }
       );
       console.log(res);
-      if (res.status === 200) {
+        if (res.status === 200) {
         toast.success(res.data.message);
+        setLoad(false)
         sessionStorage.setItem("token", res.data.token);
         sessionStorage.setItem("role", res.data.role);
         if (res.data.role === "admin") navigate("/dashboard");
@@ -35,36 +37,38 @@ function Signin() {
   };
 
   return (
-    <>
-      <div
-        className="container p-5"
-        style={{
-          width: "500px",
-          border: "1px solid black",
-          marginTop: "100px",
-        }}
-      >
-        <h2 className="Signin-head p-3">Signin to Continue</h2>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" name="email" />
-          </Form.Group>
+    <>{
+      load? <h1>LOADING.....</h1>:<div
+      className="container p-5"
+      style={{
+        width: "500px",
+        border: "1px solid black",
+        marginTop: "100px",
+      }}
+    >
+      <h2 className="Signin-head p-3">Signin to Continue</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" name="email" />
+        </Form.Group>
 
-          <Form.Group className="mb-3">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              name="password"
-            />
-          </Form.Group>
-          <Form.Group className="mb-3"></Form.Group>
-          <Button variant="primary" type="submit">
-            Login
-          </Button>
-        </Form>
-      </div>
+        <Form.Group className="mb-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3"></Form.Group>
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+    </div>
+    }
+      
     </>
   );
 }
